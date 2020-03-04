084bb438f3811ab704d42892785ca5bf3bc7d324
Austin Abbott <magicaustin51547@gmail.com>
2020-03-03 19:26:02 -0700

    Meet bare minimum requirements

--- db/pgconfig.js
+++ db/pgconfig.js
@@ -13,8 +13,23 @@ function addMessage(message) {
 
   return pool.query('INSERT INTO messages(name, message) VALUES($1, $2)', values)
 };
-// the actual code for the methods goes here
+
+function updateMessage(updateData) {
+  let values = [updateData.id, updateData.name, updateData.message];
+  return pool.query('UPDATE messages SET name=$2, message=$3 WHERE id=$1', values)
+};
+
+function deleteAMessage(index) {
+  let values = [index.id];
+  return pool.query('DELETE FROM messages WHERE id=$1', values)
+};
+
+function getOneMessage(messageIndex) {
+  let values = [messageIndex.id];
+  return pool.query('SELECT * FROM messages WHERE id=$1', values)
+  .then(data => data)
+};
 
 
 // change this to export the methods we create
-module.exports = { returnAllMessages, addMessage }; // export all methods here
+module.exports = { returnAllMessages, addMessage, updateMessage, deleteAMessage, getOneMessage }; // export all methods here
--- server.js
+++ server.js
@@ -8,17 +8,38 @@ app.use(bodyParser.json());
 
 // create a message path: /api/messages
 // Retrieve a list of all messages (output should be an array of objects): /api/messages
-app.get('/api/messages', function (req, res) {
+
+app.get('/api/messages', function (req, res) { // working
   methods.returnAllMessages()
     .then(data => {
       res.send(data)
     })
+    .catch(err => console.error(err))
 });
 
-app.post('/api/messages', function(req, res) {
+app.post('/api/messages', function(req, res) { // working
   methods.addMessage(req.body)
   .then(() => res.sendStatus(201))
+  .catch(err => console.error(err))
 });
+
+app.put('/api/messages/1', function(req, res) { // working
+  methods.updateMessage(req.body)
+  .then(() => res.sendStatus(201))
+  .catch(err => console.error(err))
+})
+
+app.delete('/api/messages/1', function(req, res) { // refactor without req.body
+  methods.deleteAMessage(req.body)
+  .then(() => res.sendStatus(201))
+  .catch(err => console.error(err))
+})
+
+app.get('/api/messages/1', function(req, res) { // refactor without req.body
+  methods.getOneMessage(req.body)
+  .then(data => res.send(data.rows))
+  .catch(err => console.error(err))
+})
 
 app.use((req, res, next) => {
   res.status(404).send('That route does not exist');
